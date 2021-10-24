7c45c90ef56bad07f39fc45c2b578d4cfc7f93b6
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-24 16:24:05 -0700

    added Json dependencies

---
+++ server.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+// Import employee class
+const Sql = require('./lib/Sql');
+// Import Insert class
+const Insert = require('./lib/Insert');
+// Import Update class
+const Update = require('./lib/Update');
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cantimploras87=',
+    database: 'employeeTracker_db'
+  });
+
+
+
+
+
+
+
+
+
